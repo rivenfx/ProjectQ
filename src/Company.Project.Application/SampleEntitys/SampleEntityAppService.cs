fc789bf89cb5c777c56a039ac20a63c99c295908
staneee <yi.hang@live.com>
2020-03-17 22:07:50 +0800

    修改调用函数

--- src/Company.Project.Application/SampleEntitys/SampleEntityAppService.cs
+++ src/Company.Project.Application/SampleEntitys/SampleEntityAppService.cs
@@ -9,15 +9,16 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Riven.Extensions;
 
 namespace Company.Project.SampleEntitys
 {
     public class SampleEntityAppService : IApplicationService
     {
-        readonly IRepository<SampleEntity, long> _repository;
+        readonly IRepository<SampleEntity> _repository;
         readonly IUnitOfWorkManager _unitOfWorkManager;
 
-        public SampleEntityAppService(IRepository<SampleEntity, long> repository, IUnitOfWorkManager unitOfWorkManager)
+        public SampleEntityAppService(IRepository<SampleEntity> repository, IUnitOfWorkManager unitOfWorkManager)
         {
             _repository = repository;
             _unitOfWorkManager = unitOfWorkManager;
@@ -25,7 +26,9 @@ namespace Company.Project.SampleEntitys
 
         public async Task<List<SampleEntity>> GetAll()
         {
-            var currentUnitOfWork = this._unitOfWorkManager.Current;
+            #region 切换数据库连接字符串/切换DbContext 用例
+            
+            //var currentUnitOfWork = this._unitOfWorkManager.Current;
 
             // 切换数据库连接字符串
             //using (currentUnitOfWork.SetConnectionStringName("TenantA"))
@@ -37,10 +40,30 @@ namespace Company.Project.SampleEntitys
             //using (currentUnitOfWork.SetDbContextProvider("TenantA"))
             //{
             //    var resultWithTenantA = await this._repository.GetAll().ToListAsync();
-            //}
+            //} 
+
+            #endregion
 
             return await _repository.GetAll().ToListAsync();
         }
+
+        public async Task<bool> Create(string name)
+        {
+            await _repository.InsertAsync(new SampleEntity()
+            {
+                DisplayName = name
+            });
+
+            return true;
+        }
+
+        public async Task<bool> Delete(long id)
+        {
+            await _repository.DeleteAsync(id);
+
+            return true;
+        }
+
 
     }
 }
