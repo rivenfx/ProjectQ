f351e839d7fb873adbee89b08ccc6b773591026d
staneee <yi.hang@live.com>
2020-03-20 22:27:45 +0800

    修改RoleStore和UserStore实现

--- src/Company.Project.Domain/Authorization/Roles/RoleStore.cs
+++ src/Company.Project.Domain/Authorization/Roles/RoleStore.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Riven;
 using Riven.Repositories;
+using Riven.Uow;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,66 +14,171 @@ namespace Company.Project.Authorization.Roles
 {
     public class RoleStore : IRoleStore<Role>
     {
+        private bool _disposed;
+
+
+        protected readonly IUnitOfWorkManager _unitOfWorkManager;
         protected readonly IRepository<Role> _roleRepo;
 
-        public RoleStore(IRepository<Role> roleRepo)
+        protected IdentityErrorDescriber ErrorDescriber { get; }
+
+        public RoleStore(IUnitOfWorkManager unitOfWorkManager, IRepository<Role> roleRepo)
         {
+            _unitOfWorkManager = unitOfWorkManager;
             _roleRepo = roleRepo;
+
+            ErrorDescriber = new IdentityErrorDescriber();
         }
 
-        public Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
+        public async Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(role, nameof(role));
+
+            await _roleRepo.InsertAsync(role);
+            await SaveChanges(cancellationToken);
+            return IdentityResult.Success;
         }
 
-        public Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken)
+        public async Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(role, nameof(role));
+
+            await this._roleRepo.DeleteAsync(role);
+            try
+            {
+                await SaveChanges(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return IdentityResult.Failed(ErrorDescriber.ConcurrencyFailure());
+            }
+            return IdentityResult.Success;
         }
 
-        public void Dispose()
+        public async Task<Role> FindByIdAsync(string roleId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+
+            var id = ConvertIdFromString(roleId);
+            return await this._roleRepo.GetAll()
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync(r => r.Id.Equals(roleId), cancellationToken);
         }
 
-        public Task<Role> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        public async Task<Role> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
-        }
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
 
-        public Task<Role> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
+            return await this._roleRepo.GetAll()
+                               .AsNoTracking()
+                               .FirstOrDefaultAsync(r => r.NormalizedName == normalizedRoleName, cancellationToken);
         }
 
         public Task<string> GetNormalizedRoleNameAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(role, nameof(role));
+
+            return Task.FromResult(role.NormalizedName);
         }
 
         public Task<string> GetRoleIdAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(role, nameof(role));
+
+            return Task.FromResult(ConvertIdToString(role.Id));
         }
 
         public Task<string> GetRoleNameAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(role, nameof(role));
+
+            return Task.FromResult(role.Name);
         }
 
         public Task SetNormalizedRoleNameAsync(Role role, string normalizedName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(role, nameof(role));
+
+            role.NormalizedName = normalizedName;
+            return Task.CompletedTask;
         }
 
         public Task SetRoleNameAsync(Role role, string roleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(role, nameof(role));
+
+            role.Name = roleName;
+            return Task.CompletedTask;
         }
 
-        public Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
+        public async Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(role, nameof(role));
+
+            role.ConcurrencyStamp = Guid.NewGuid().ToString();
+            await this._roleRepo.UpdateAsync(role);
+            try
+            {
+                await SaveChanges(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return IdentityResult.Failed(ErrorDescriber.ConcurrencyFailure());
+            }
+            return IdentityResult.Success;
+        }
+        public void Dispose()
+        {
+            _disposed = true;
+        }
+
+        protected virtual void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
+
+        protected virtual async Task SaveChanges(CancellationToken cancellationToken)
+        {
+            await this._unitOfWorkManager.Current.SaveChangesAsync(cancellationToken);
+        }
+
+        protected virtual string ConvertIdToString(long id)
+        {
+            if (object.Equals(id, default(long)))
+            {
+                return null;
+            }
+            return id.ToString();
+        }
+
+        protected virtual long ConvertIdFromString(string id)
+        {
+            if (id == null)
+            {
+                return default(long);
+            }
+            return (long)TypeDescriptor.GetConverter(typeof(long)).ConvertFromInvariantString(id);
         }
     }
 }
--- src/Company.Project.Domain/Authorization/Users/UserStore.cs
+++ src/Company.Project.Domain/Authorization/Users/UserStore.cs
@@ -8,34 +8,61 @@ using System.Threading.Tasks;
 using Riven.Repositories;
 using Riven;
 using Microsoft.EntityFrameworkCore;
+using Riven.Uow;
 
 namespace Company.Project.Authorization.Users
 {
     public class UserStore : IUserStore<User>
     {
+        private bool _disposed;
+
+        protected readonly IUnitOfWorkManager _unitOfWorkManager;
+
         protected readonly IRepository<User> _userRepo;
 
-        public UserStore(IRepository<User> userRepo)
+        protected IdentityErrorDescriber ErrorDescriber { get; }
+
+        public UserStore(IUnitOfWorkManager unitOfWorkManager, IRepository<User> userRepo)
         {
+            _unitOfWorkManager = unitOfWorkManager;
             _userRepo = userRepo;
+
+            ErrorDescriber = new IdentityErrorDescriber();
         }
 
-
-        public Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
+        public async Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
         {
-            //return _userRepo.InsertAsync(user);
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            ThrowIfDisposed();
+
+            Check.NotNull(user, nameof(user));
+
+            await _userRepo.InsertAsync(user);
+            await SaveChanges(cancellationToken);
+
+            return IdentityResult.Success;
         }
 
-        public Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
+        public async Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(user, nameof(user));
+
+            await _userRepo.DeleteAsync(user.Id);
+            try
+            {
+                await SaveChanges(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return IdentityResult.Failed(ErrorDescriber.ConcurrencyFailure());
+            }
+            return IdentityResult.Success;
         }
 
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+
 
         public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
         {
@@ -54,37 +81,101 @@ namespace Company.Project.Authorization.Users
 
         public Task<User> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+
+            return _userRepo.GetAll()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.NormalizedUserName == normalizedUserName, cancellationToken);
         }
 
         public Task<string> GetNormalizedUserNameAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(user, nameof(user));
+            return Task.FromResult(user.NormalizedUserName);
         }
 
         public Task<string> GetUserIdAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(user, nameof(user));
+            return Task.FromResult(ConvertIdToString(user.Id));
         }
 
         public Task<string> GetUserNameAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(user, nameof(user));
+            return Task.FromResult(user.UserName);
         }
 
         public Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(user, nameof(user));
+            user.NormalizedUserName = normalizedName;
+            return Task.CompletedTask;
         }
 
         public Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(user, nameof(user));
+            user.UserName = userName;
+            return Task.CompletedTask;
         }
 
-        public Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
+        public async Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            Check.NotNull(user, nameof(user));
+
+            
+            user.ConcurrencyStamp = Guid.NewGuid().ToString();
+            await this._userRepo.UpdateAsync(user);
+            try
+            {
+                await SaveChanges(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return IdentityResult.Failed(ErrorDescriber.ConcurrencyFailure());
+            }
+            return IdentityResult.Success;
+        }
+
+        public void Dispose()
+        {
+            _disposed = true;
+        }
+
+        protected virtual void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
+
+        protected virtual async Task SaveChanges(CancellationToken cancellationToken)
+        {
+            await this._unitOfWorkManager.Current.SaveChangesAsync(cancellationToken);
+        }
+
+        protected virtual string ConvertIdToString(long id)
+        {
+            if (object.Equals(id, default(long)))
+            {
+                return null;
+            }
+            return id.ToString();
         }
     }
 }
