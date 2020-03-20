ca1af422ea7d5fa032882e93d767586942b72113
staneee <yi.hang@live.com>
2020-03-20 13:50:21 +0800

    日志功能

--- src/Company.Project.Host/Program.cs
+++ src/Company.Project.Host/Program.cs
@@ -9,15 +9,40 @@ using Microsoft.Extensions.Logging;
 
 using WebHost = Microsoft.Extensions.Hosting.Host;
 
+using Serilog;
+using Serilog.Events;
+
+
 namespace Company.Project
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args)
-                .Build()
-                .Run();
+            Log.Logger = new LoggerConfiguration()
+#if DEBUG
+                .MinimumLevel.Debug()
+#else
+                .MinimumLevel.Information()
+#endif
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File("Logs/logs.txt")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Company.Project.Host.");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -26,7 +51,8 @@ namespace Company.Project
                             .ConfigureWebHostDefaults(webBuilder =>
                             {
                                 webBuilder.UseStartup<Startup>();
-                            });
+                            })
+                            .UseSerilog();
         }
 
     }
