d209e5c4df2610f4c310f84fa399f7f1a3898db0
staneee <yi.hang@live.com>
2020-03-21 09:24:28 +0800

    移动Store位置

---
+++ src/Company.Project.EntityFrameworkCore/Authorization/Roles/RoleStore.cs
@@ -0,0 +1,25 @@
+﻿using Company.Project.Authorization.Users;
+using Company.Project.Database;
+using Microsoft.AspNetCore.Identity;
+using Riven.Extensions;
+using Riven.Identity.Roles;
+using Riven.Uow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.Authorization.Roles
+{
+    public class RoleStore : AppRoleStore<Role, AppDbContext, long, UserRole, RoleClaim>
+    {
+        protected readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public RoleStore(IUnitOfWorkManager unitOfWorkManager, IdentityErrorDescriber describer = null)
+            : base()
+        {
+            this._unitOfWorkManager = unitOfWorkManager;
+        }
+
+        public override AppDbContext Context => _unitOfWorkManager.Current.GetDbContext() as AppDbContext;
+    }
+}
