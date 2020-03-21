23393ed6957639904bdb5f7bba701121a4c3942c
staneee <yi.hang@live.com>
2020-03-21 17:54:09 +0800

    修改ISeeder位置

--- src/Company.Project.EntityFrameworkCore/SeedData/ISeeder.cs
+++ src/Company.Project.EntityFrameworkCore/SeedData/ISeeder.cs
@@ -1,13 +1,8 @@
-﻿using Company.Project.Authorization.Users;
 using Riven.Repositories;
-using Riven.Uow;
-using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
-using Company.Project.Authorization;
 using Microsoft.AspNetCore.Identity;
 
 namespace Company.Project.SeedData
@@ -15,87 +10,5 @@ namespace Company.Project.SeedData
     public interface ISeeder
     {
         void Create();
-    }
-
-    public class Seeder : ISeeder
-    {
-        protected readonly IServiceProvider _serviceProvider;
-        protected readonly IUnitOfWorkManager _unitOfWorkManager;
-        protected readonly UserPasswordHasher _passwordHasher;
-
-
-        public Seeder(IServiceProvider serviceProvider, IUnitOfWorkManager unitOfWorkManager, UserPasswordHasher passwordHasher)
-        {
-            _serviceProvider = serviceProvider;
-            _unitOfWorkManager = unitOfWorkManager;
-            _passwordHasher = passwordHasher;
-        }
-
-        public void Create()
-        {
-            using (var uow = _unitOfWorkManager.Begin())
-            {
-                try
-                {
-                    this.CreateUsers();
-
-
-                    uow.Complete();
-                }
-                catch (Exception e)
-                {
-                    throw e;
-                }
-            }
-        }
-
-
-        protected void CreateUsers()
-        {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var userManager = scope.ServiceProvider.GetService<UserManager>();
-                var adminUser = userManager.FindByNameOrEmailOrPhoneNumberAsync("admin").Result;
-                if (adminUser == null)
-                {
-                    adminUser = new User();
-
-                    adminUser.UserName = "admin";
-                    adminUser.PhoneNumber = "13028166007";
-                    adminUser.PhoneNumberConfirmed = true;
-                    adminUser.Email = "yi.hang@live.com";
-                    adminUser.EmailConfirmed = true;
-                    adminUser.LockoutEnabled = false;
-                    adminUser.TwoFactorEnabled = false;
-                    adminUser.IsActive = true;
-
-                    var identityResult = userManager.CreateAsync(adminUser, "123qwe").Result;
-                }
-            }
-
-            //    var userRepo = _serviceProvider.GetService<IRepository<User>>();
-            //var adminUser = userRepo.GetAll().FirstOrDefault(o => o.UserName == "admin");
-            //if (adminUser == null)
-            //{
-            //    adminUser = new User();
-
-            //    adminUser.UserName = "admin";
-            //    adminUser.PhoneNumber = "13028166007";
-            //    adminUser.PhoneNumberConfirmed = true;
-            //    adminUser.Email = "yi.hang@live.com";
-            //    adminUser.EmailConfirmed = true;
-            //    adminUser.LockoutEnabled = false;
-            //    adminUser.TwoFactorEnabled = false;
-            //    adminUser.IsActive = true;
-
-            //    adminUser.NormalizedUserName = adminUser.UserName.ToLower();
-            //    adminUser.NormalizedEmail = adminUser.Email.ToLower();
-
-
-            //    adminUser.PasswordHash = this._passwordHasher.HashPassword(adminUser, "123qwe");
-            //    userRepo.InsertAsync(adminUser);
-            //}
-
-        }
     }
 }
---
+++ src/Company.Project.EntityFrameworkCore/SeedData/Seeder.cs
@@ -0,0 +1,90 @@
+﻿using Company.Project.Authorization.Users;
+using Riven.Uow;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Company.Project.Authorization;
+
+namespace Company.Project.SeedData
+{
+    public class Seeder : ISeeder
+    {
+        protected readonly IServiceProvider _serviceProvider;
+        protected readonly IUnitOfWorkManager _unitOfWorkManager;
+        protected readonly UserPasswordHasher _passwordHasher;
+
+
+        public Seeder(IServiceProvider serviceProvider, IUnitOfWorkManager unitOfWorkManager, UserPasswordHasher passwordHasher)
+        {
+            _serviceProvider = serviceProvider;
+            _unitOfWorkManager = unitOfWorkManager;
+            _passwordHasher = passwordHasher;
+        }
+
+        public void Create()
+        {
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                try
+                {
+                    this.CreateUsers();
+
+
+                    uow.Complete();
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+
+
+        protected void CreateUsers()
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetService<UserManager>();
+                var adminUser = userManager.FindByNameOrEmailOrPhoneNumberAsync("admin").Result;
+                if (adminUser == null)
+                {
+                    adminUser = new User();
+
+                    adminUser.UserName = "admin";
+                    adminUser.PhoneNumber = "13028166007";
+                    adminUser.PhoneNumberConfirmed = true;
+                    adminUser.Email = "yi.hang@live.com";
+                    adminUser.EmailConfirmed = true;
+                    adminUser.LockoutEnabled = false;
+                    adminUser.TwoFactorEnabled = false;
+                    adminUser.IsActive = true;
+
+                    var identityResult = userManager.CreateAsync(adminUser, "123qwe").Result;
+                }
+            }
+
+            //    var userRepo = _serviceProvider.GetService<IRepository<User>>();
+            //var adminUser = userRepo.GetAll().FirstOrDefault(o => o.UserName == "admin");
+            //if (adminUser == null)
+            //{
+            //    adminUser = new User();
+
+            //    adminUser.UserName = "admin";
+            //    adminUser.PhoneNumber = "13028166007";
+            //    adminUser.PhoneNumberConfirmed = true;
+            //    adminUser.Email = "yi.hang@live.com";
+            //    adminUser.EmailConfirmed = true;
+            //    adminUser.LockoutEnabled = false;
+            //    adminUser.TwoFactorEnabled = false;
+            //    adminUser.IsActive = true;
+
+            //    adminUser.NormalizedUserName = adminUser.UserName.ToLower();
+            //    adminUser.NormalizedEmail = adminUser.Email.ToLower();
+
+
+            //    adminUser.PasswordHash = this._passwordHasher.HashPassword(adminUser, "123qwe");
+            //    userRepo.InsertAsync(adminUser);
+            //}
+
+        }
+    }
+}
