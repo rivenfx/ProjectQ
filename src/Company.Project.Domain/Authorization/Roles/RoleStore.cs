86a4cf95310c902a9a1836a985716a139844f75d
staneee <yi.hang@live.com>
2020-03-21 09:14:35 +0800

    增加新的Store实现

---
+++ src/Company.Project.Domain/Authorization/Roles/RoleStore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.Authorization.Roles
+{
+    public class RoleStore
+    {
+    }
+}
---
+++ src/Company.Project.Domain/Authorization/Users/UserStore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.Authorization.Users
+{
+    public class UserStore
+    {
+    }
+}
