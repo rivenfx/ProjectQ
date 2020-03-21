a4632c1947c8266aad771070ae8db7a5aa406925
staneee <yi.hang@live.com>
2020-03-21 18:09:59 +0800

    修改表配置

--- src/Company.Project.EntityFrameworkCore/Database/AppDbContext.cs
+++ src/Company.Project.EntityFrameworkCore/Database/AppDbContext.cs
@@ -16,11 +16,6 @@ namespace Company.Project.Database
     public class AppDbContext
         : IdentityDbContext<User, Role, long, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
     {
-        public AppDbContext()
-            : base()
-        {
-
-        }
 
         public AppDbContext(DbContextOptions options)
             : base(options)
@@ -29,5 +24,53 @@ namespace Company.Project.Database
         }
 
         public DbSet<SampleEntity> SampleEntitys { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            ConfigurationIdentityTables(builder);
+        }
+
+
+        /// <summary>
+        /// 配置Identity的表
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        protected virtual ModelBuilder ConfigurationIdentityTables(ModelBuilder builder)
+        {
+            builder.Entity<Role>((entityBuilder) =>
+            {
+                entityBuilder.ToTable(nameof(Role));
+            });
+            builder.Entity<RoleClaim>((entityBuilder) =>
+            {
+                entityBuilder.ToTable(nameof(RoleClaim));
+            });
+
+            builder.Entity<User>((entityBuilder) =>
+            {
+                entityBuilder.ToTable(nameof(User));
+            });
+            builder.Entity<UserClaim>((entityBuilder) =>
+            {
+                entityBuilder.ToTable(nameof(UserClaim));
+            });
+            builder.Entity<UserLogin>((entityBuilder) =>
+            {
+                entityBuilder.ToTable(nameof(UserLogin));
+            });
+            builder.Entity<UserToken>((entityBuilder) =>
+            {
+                entityBuilder.ToTable(nameof(UserToken));
+            });
+            builder.Entity<UserRole>((entityBuilder) =>
+            {
+                entityBuilder.ToTable(nameof(UserRole));
+            });
+
+            return builder;
+        }
     }
 }
