6bb8ea4b94fd7a0008572a7583054da3d2086c53
staneee <yi.hang@live.com>
2020-03-22 00:13:27 +0800

    引入功能脚本

---
+++ src/Company.Project.Host/wwwroot/scripts/form-extensions.js
@@ -0,0 +1,19 @@
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+}
+
+$.fn.serializeObjectStr = function () {
+    return JSON.stringify(this.serializeObject())
+}
