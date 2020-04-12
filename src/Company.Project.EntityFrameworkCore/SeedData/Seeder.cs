4d7dc4677f17460c95b6b0c4095dc21ac3fd5df5
staneee <yi.hang@live.com>
2020-04-12 21:42:10 +0800

    移除不用的代码

--- src/Company.Project.EntityFrameworkCore/SeedData/Seeder.cs
+++ src/Company.Project.EntityFrameworkCore/SeedData/Seeder.cs
@@ -10,33 +10,16 @@ namespace Company.Project.SeedData
     public class Seeder : ISeeder, ITransientDependency
     {
         protected readonly IServiceProvider _serviceProvider;
-        protected readonly IUnitOfWorkManager _unitOfWorkManager;
-        protected readonly UserPasswordHasher _passwordHasher;
 
 
-        public Seeder(IServiceProvider serviceProvider, IUnitOfWorkManager unitOfWorkManager, UserPasswordHasher passwordHasher)
+        public Seeder(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
-            _unitOfWorkManager = unitOfWorkManager;
-            _passwordHasher = passwordHasher;
         }
 
         public void Create()
         {
-            using (var uow = _unitOfWorkManager.Begin())
-            {
-                try
-                {
-                    this.CreateUsers();
-
-
-                    uow.Complete();
-                }
-                catch (Exception e)
-                {
-                    throw e;
-                }
-            }
+            this.CreateUsers();
         }
 
 
@@ -62,30 +45,6 @@ namespace Company.Project.SeedData
                     var identityResult = userManager.CreateAsync(adminUser, "123qwe").Result;
                 }
             }
-
-            //    var userRepo = _serviceProvider.GetService<IRepository<User>>();
-            //var adminUser = userRepo.GetAll().FirstOrDefault(o => o.UserName == "admin");
-            //if (adminUser == null)
-            //{
-            //    adminUser = new User();
-
-            //    adminUser.UserName = "admin";
-            //    adminUser.PhoneNumber = "13028166007";
-            //    adminUser.PhoneNumberConfirmed = true;
-            //    adminUser.Email = "yi.hang@live.com";
-            //    adminUser.EmailConfirmed = true;
-            //    adminUser.LockoutEnabled = false;
-            //    adminUser.TwoFactorEnabled = false;
-            //    adminUser.IsActive = true;
-
-            //    adminUser.NormalizedUserName = adminUser.UserName.ToLower();
-            //    adminUser.NormalizedEmail = adminUser.Email.ToLower();
-
-
-            //    adminUser.PasswordHash = this._passwordHasher.HashPassword(adminUser, "123qwe");
-            //    userRepo.InsertAsync(adminUser);
-            //}
-
         }
     }
 }
