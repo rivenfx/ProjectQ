f5798a4ea7e8fa59c8ced282ccbad1d7a5e195f1
staneee <yi.hang@live.com>
2020-05-05 12:31:05 +0800

    增加数据库迁移

--- src/Company.Project.EntityFrameworkCore/Migrations/20200505043013_ChangeRoleEntity.Designer.cs
+++ src/Company.Project.EntityFrameworkCore/Migrations/20200505043013_ChangeRoleEntity.Designer.cs
@@ -4,14 +4,16 @@ using Company.Project.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Company.Project.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200505043013_ChangeRoleEntity")]
+    partial class ChangeRoleEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
