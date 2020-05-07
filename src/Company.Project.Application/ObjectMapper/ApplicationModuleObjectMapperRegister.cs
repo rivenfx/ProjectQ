fd46afb916a80372b7744d1fea8e0beff5059d8d
staneee <yi.hang@live.com>
2020-05-07 22:11:00 +0800

    增加映射器

---
+++ src/Company.Project.Application/ObjectMapper/ApplicationModuleObjectMapperRegister.cs
@@ -0,0 +1,24 @@
+﻿using Company.Project.Authorization.Users;
+using Company.Project.Authorization.Users.Dtos;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.ObjectMapper
+{
+    /// <summary>
+    /// Application模块的对象映射器
+    /// </summary>
+    public class ApplicationModuleObjectMapperRegister : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            //config.NewConfig<User, UserEditDto>()
+            //    .TwoWays() // 双向
+            //    .Map((dest) => dest.UserName, (src) => src.UserName ?? string.Empty); // 指定字段映射
+
+
+        }
+    }
+}
