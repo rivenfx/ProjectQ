d6fee418a0234fa1065c8792e8491ae4894828d2
staneee <yi.hang@live.com>
2020-05-23 10:07:51 +0800

    修改初始化

--- ui/company-project-angular-ui/src/app/shared/riven/riven.module.ts
+++ ui/company-project-angular-ui/src/app/shared/riven/riven.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { SessionService } from './session.service';
+import { PermissionCheckerService } from './permission-checker.service';
+import { AuthService } from './auth.service';
+import { AppInfoService } from './app-info.service';
 
 ;
 
 @NgModule({
   providers: [
     SessionService,
+    PermissionCheckerService,
+    AuthService,
+    AppInfoService,
   ],
 })
 export class RivenModule {
