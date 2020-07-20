8572ee78155309d1560bd9e21a8504a1007fab2e
staneee <yi.hang@live.com>
2020-07-20 22:47:20 +0800

    修改AppDbContext

--- src/Company.Project.EntityFrameworkCore/Database/AppDbContext.cs
+++ src/Company.Project.EntityFrameworkCore/Database/AppDbContext.cs
@@ -1,78 +1,117 @@
+using Company.Project.Authorization;
 using Company.Project.Authorization.Roles;
 using Company.Project.Authorization.Users;
 using Company.Project.Samples;
+
+using JetBrains.Annotations;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
 using Riven;
 using Riven.Identity.Roles;
 using Riven.Identity.Users;
+
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
 using System.Text;
 
 namespace Company.Project.Database
 {
     public class AppDbContext
-        : IdentityDbContext<User, Role, long, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+        : IdentityDbContext<User, Role, long, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>,
+        IRivenDbContext,
+        IRivenFilterDbContext,
+        IRivenAuditedDbContext
     {
+        [NotMapped]
+        public IServiceProvider ServiceProvider { get; }
 
-        public AppDbContext(DbContextOptions options)
+        [NotMapped]
+        public ConcurrentDictionary<Type, object> SerivceInstanceMap { get; } = new ConcurrentDictionary<Type, object>();
+
+        [NotMapped]
+        public IAppSession AppSession => ServiceProvider?.GetService<IAppSession>();
+
+        public AppDbContext(DbContextOptions options, IServiceProvider serviceProvider = null)
             : base(options)
         {
-
+            ServiceProvider = serviceProvider;
         }
 
         public DbSet<SampleEntity> SampleEntitys { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
 
-            ConfigurationIdentityTables(builder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            ConfigurationIdentityTables(modelBuilder);
         }
 
 
         /// <summary>
         /// 配置Identity的表
         /// </summary>
-        /// <param name="builder"></param>
+        /// <param name="modelBuilder"></param>
         /// <returns></returns>
-        protected virtual ModelBuilder ConfigurationIdentityTables(ModelBuilder builder)
+        protected virtual ModelBuilder ConfigurationIdentityTables(ModelBuilder modelBuilder)
         {
-            builder.Entity<Role>((entityBuilder) =>
+            modelBuilder.Entity<Role>((entityBuilder) =>
             {
                 entityBuilder.ToTable($"{nameof(Role)}s");
             });
-            builder.Entity<RoleClaim>((entityBuilder) =>
+            modelBuilder.Entity<RoleClaim>((entityBuilder) =>
             {
                 entityBuilder.ToTable($"{nameof(RoleClaim)}s");
             });
 
-            builder.Entity<User>((entityBuilder) =>
+            modelBuilder.Entity<User>((entityBuilder) =>
             {
                 entityBuilder.ToTable($"{nameof(User)}s");
                 entityBuilder.HasIndex(o => o.Nickname)
                         .IsUnique();
             });
-            builder.Entity<UserClaim>((entityBuilder) =>
+            modelBuilder.Entity<UserClaim>((entityBuilder) =>
             {
                 entityBuilder.ToTable($"{nameof(UserClaim)}s");
             });
-            builder.Entity<UserLogin>((entityBuilder) =>
+            modelBuilder.Entity<UserLogin>((entityBuilder) =>
             {
                 entityBuilder.ToTable($"{nameof(UserLogin)}s");
             });
-            builder.Entity<UserToken>((entityBuilder) =>
+            modelBuilder.Entity<UserToken>((entityBuilder) =>
             {
                 entityBuilder.ToTable($"{nameof(UserToken)}s");
             });
-            builder.Entity<UserRole>((entityBuilder) =>
+            modelBuilder.Entity<UserRole>((entityBuilder) =>
             {
                 entityBuilder.ToTable($"{nameof(UserRole)}s");
             });
 
-            return builder;
+            return modelBuilder;
+        }
+
+
+        public bool GetMultiTenancyEnabled()
+        {
+            return true;
+        }
+
+        public string GetCurrentTenantNameOrNull()
+        {
+            return AppSession?.TenantIdString;
+        }
+
+        public string GetCurrentUserIdOrNull()
+        {
+            return AppSession?.UserIdString;
         }
     }
 }
