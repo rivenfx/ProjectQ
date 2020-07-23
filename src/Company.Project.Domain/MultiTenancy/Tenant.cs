b40925794f1f71f3151f43fda1046e34931250c8
staneee <yi.hang@live.com>
2020-07-23 14:36:51 +0800

    增加租户实体

---
+++ src/Company.Project.Domain/MultiTenancy/Tenant.cs
@@ -0,0 +1,15 @@
+using Riven.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.MultiTenancy
+{
+    public class Tenant : Entity<Guid>
+    {
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
