505ad137694b2cdff23c686c9586b18d5161af3b
staneee <yi.hang@live.com>
2020-07-24 15:14:19 +0800

    修改加载数据库连接字符串

--- src/Company.Project.EntityFrameworkCore/Database/AppDesignTimeDbContextFactory.cs
+++ src/Company.Project.EntityFrameworkCore/Database/AppDesignTimeDbContextFactory.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore.Design;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Company.Project.Configuration;
 
 namespace Company.Project.Database
 {
@@ -10,14 +13,28 @@ namespace Company.Project.Database
     {
         public AppDbContext CreateDbContext(string[] args)
         {
-            var connectionString = "server=(localdb)\\MSSQLLocalDB;database=RivenProjectQ;uid=sa;pwd=123;";
+            var configuration = BuildConfiguration();
 
+            var connectionString = configuration.GetDefaultDatabaseConnectionString();
+
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} 开始迁移");
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} 当前使用连接字符串: {connectionString}");
 
             var builder = new DbContextOptionsBuilder<AppDbContext>();
 
             AppDbContextConfigurer.Configure(builder, connectionString);
 
             return new AppDbContext(builder.Options);
+        }
+
+
+        private static IConfiguration BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            return builder.Build();
         }
     }
 }
