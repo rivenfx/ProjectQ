e75c735588e83d54fdc4b5676c8ce1616fe5f039
staneee <yi.hang@live.com>
2020-07-26 20:30:30 +0800

    修改接口

--- src/Company.Project.Application/MultiTenancy/TenantAppService.cs
+++ src/Company.Project.Application/MultiTenancy/TenantAppService.cs
@@ -1,6 +1,7 @@
 ﻿using Company.Project.MultiTenancy.Dtos;
 
 using Riven.Application;
+using Riven.Uow;
 
 using System;
 using System.Collections.Generic;
@@ -11,30 +12,39 @@ namespace Company.Project.MultiTenancy
 {
     public class TenantAppService : IApplicationService
     {
+        readonly IUnitOfWorkManager _unitOfWorkManager;
         readonly ITenantManager _tenantManager;
 
-        public TenantAppService(ITenantManager tenantManager)
+        public TenantAppService(IUnitOfWorkManager unitOfWorkManager, ITenantManager tenantManager)
         {
+            _unitOfWorkManager = unitOfWorkManager;
             _tenantManager = tenantManager;
         }
 
-        public async Task<IsTenantAvailableOutput> IsTenantAvailable(IsTenantAvailableInput input)
+        [UnitOfWork(IsDisabled = true)]
+        public virtual async Task<IsTenantAvailableOutput> IsTenantAvailable(IsTenantAvailableInput input)
         {
-            var tenant = await _tenantManager.GetByName(input.TenantName);
-
-            var result = new IsTenantAvailableOutput();
-            result.TenantName = tenant?.Name;
-
-            if (tenant == null)
+            using (var uow = _unitOfWorkManager.Begin())
             {
-                result.State = TenantAvailabilityState.NotFound;
-            }
-            else
-            {
-                result.State = tenant.IsActive ? TenantAvailabilityState.Available : TenantAvailabilityState.InActive;
+
+                var tenant = await _tenantManager.GetByName(input.TenantName);
+
+                var result = new IsTenantAvailableOutput();
+                result.TenantName = tenant?.Name;
+
+                if (tenant == null)
+                {
+                    result.State = TenantAvailabilityState.NotFound;
+                }
+                else
+                {
+                    result.State = tenant.IsActive ? TenantAvailabilityState.Available : TenantAvailabilityState.InActive;
+                }
+
+                return result;
+
             }
 
-            return result;
         }
 
     }
