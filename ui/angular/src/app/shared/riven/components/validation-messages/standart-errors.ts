d4f3d6003625126ba6db7922435dbf741c620b58
staneee <yi.hang@live.com>
2020-08-08 16:01:45 +0800

    错误验证消息组件修改

--- ui/angular/src/app/shared/riven/components/validation-messages/standart-errors.ts
+++ ui/angular/src/app/shared/riven/components/validation-messages/standart-errors.ts
@@ -2,16 +2,12 @@ import { IErrorDef } from './interfaces';
 
 const standartErrors: IErrorDef[] = [
   { error: 'required', localizationKey: 'validation.required' },
-  { error: 'minlength', localizationKey: 'validation.minlength', errorProperty: 'requiredLength' },
-  {
-    error: 'maxlength',
-    localizationKey: 'validation.maxlength',
-    errorProperty: 'requiredLength',
-  },
   { error: 'email', localizationKey: 'validation.email' },
-  { error: 'pattern', localizationKey: 'validation.pattern', errorProperty: 'requiredPattern' },
-  { error: 'confirm', localizationKey: 'validation.confirm', errorProperty: 'requiredPattern' },
+  { error: 'confirm', localizationKey: 'validation.confirm' },
   { error: 'exist', localizationKey: 'validation.exist' },
+  { error: 'minlength', localizationKey: 'validation.minlength', errorProperty: 'requiredLength' },
+  { error: 'maxlength', localizationKey: 'validation.maxlength', errorProperty: 'requiredLength' },
+  { error: 'pattern', localizationKey: 'validation.pattern', errorProperty: 'requiredPattern' },
 ];
 
 export default standartErrors;
--- ui/angular/src/app/shared/riven/components/validation-messages/validation-messages.component.ts
+++ ui/angular/src/app/shared/riven/components/validation-messages/validation-messages.component.ts
@@ -1,54 +1,102 @@
-import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef,
+  Component,
+  Injector,
+  Input,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
 import { IErrorDef } from './interfaces';
 import { FormControl } from '@angular/forms';
+import { SampleComponentBase } from '@shared/common';
 import standartErrors from './standart-errors';
-
 
 @Component({
   selector: 'validation-messages',
   templateUrl: './validation-messages.component.html',
   styleUrls: ['./validation-messages.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ValidationMessagesComponent
+export class ValidationMessagesComponent extends SampleComponentBase
   implements OnChanges {
 
+  private _formCtrlInited = false;
   /** 表单控件 */
   @Input() formCtrl: FormControl;
-
   /** 自定义错误 */
   @Input() customErrors: IErrorDef[];
+  /** 所有异常 */
+  errorSource: any = {};
+  /** 页面异常信息 */
+  viewErrors: string[] = [];
 
-  /** 所有的错误 */
-  errorDefsInternal: IErrorDef[] = standartErrors;
+  constructor(
+    injector: Injector,
+    private cdr: ChangeDetectorRef,
+  ) {
+    super(injector);
 
-
-  constructor() {
+    this.updateErrorSource();
   }
 
   ngOnChanges(changes: { [P in keyof this]?: SimpleChange } & SimpleChanges) {
     if (changes.customErrors) {
-      this.updateErrorDefsInternal();
+      this.updateErrorSource();
+    }
+    if (changes.formCtrl && changes.formCtrl.currentValue && !this._formCtrlInited) {
+      this._formCtrlInited = true;
+      const self = this;
+      self.updateViewError();
+      changes.formCtrl.currentValue.valueChanges.subscribe((res) => {
+        self.updateViewError();
+      });
     }
   }
 
-  updateErrorDefsInternal() {
+  /** 更新所有定义的错误 */
+  private updateErrorSource() {
     if (!this.customErrors || this.customErrors.length === 0) {
-      return standartErrors;
+      standartErrors.forEach((item) => {
+        this.errorSource[item.error] = item;
+      });
+      return;
     }
+
     const standarts = standartErrors.filter(stdErr => !this.customErrors.every(customErr => customErr.error === stdErr.error));
-    this.errorDefsInternal = standarts.concat(this.customErrors);
+    standarts.concat(this.customErrors)
+      .forEach((item) => {
+        this.errorSource[item.error] = item;
+      });
   }
 
+  /** 更新视图错误 */
+  private updateViewError() {
+    console.log(this.formCtrl);
+    if (this.formCtrl.invalid && (this.formCtrl.dirty || this.formCtrl.touched)) {
+      const tmpViewErrors = [];
+      for (const key in this.formCtrl.errors) {
+        const formError = this.formCtrl.errors[key];
+        if (!formError) {
+          continue;
+        }
+        const error: IErrorDef = this.errorSource[key];
+        if (!error) {
+          continue;
+        }
 
-  getErrorDefinitionIsInValid(errorDef: IErrorDef): boolean {
-    return !!this.formCtrl.errors[errorDef.error];
-  }
+        const errorRequirement = formError[error.errorProperty];
+        tmpViewErrors.push(
+          !!errorRequirement ? this.l(error.localizationKey) + ' ' + errorRequirement : this.l(error.localizationKey),
+        );
+      }
 
-  getErrorDefinitionMessage(errorDef: IErrorDef): string {
-    let errorRequirement = this.formCtrl.errors[errorDef.error][errorDef.errorProperty];
-    return !!errorRequirement ? errorDef.localizationKey + errorRequirement : errorDef.localizationKey;
-    // return !!errorRequirement
-    //   ? this.appLocalizationService.l(errorDef.localizationKey, errorRequirement)
-    //   : this.appLocalizationService.l(errorDef.localizationKey);
+      const newError = tmpViewErrors.join(',');
+      const oldError = this.viewErrors.join(',');
+      if (newError !== oldError) {
+        this.viewErrors = tmpViewErrors;
+        this.cdr.detectChanges();
+      }
+    }
   }
 }
