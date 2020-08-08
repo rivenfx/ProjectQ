4724b5553b3b8f6b35333605ef76464d5ae0e734
staneee <yi.hang@live.com>
2020-08-08 11:21:02 +0800

    模态框基类修改

--- ui/angular/src/app/shared/common/modal-component-base.ts
+++ ui/angular/src/app/shared/common/modal-component-base.ts
@@ -3,15 +3,46 @@ import { Injector, Input } from '@angular/core';
 import { NzModalRef } from 'ng-zorro-antd';
 
 export abstract class ModalComponentBase<T> extends AppComponentBase {
+
+  private _modalInput: T;
+  private _isEdit: boolean;
+  private _readonly: boolean;
+
+  /** 标题前缀 */
+  titlePrefix = this.l('label.create');
+  /** 标题 */
   title = '';
+  /** 模态框指针 */
   modalRef: NzModalRef;
 
+  /** 外部输入参数 */
   @Input()
-  modalInput: T;
+  set modalInput(val: T) {
+    this._modalInput = val;
+    if (!!val && !this.readonly) {
+      this.titlePrefix = this.l('label.edit');
+    }
+  }
+  /** 外部输入参数 */
+  get modalInput(): T {
+    return this._modalInput;
+  }
 
-  /** 是否为编辑 */
+  /** 只读 */
+  set readonly(val: boolean) {
+    this._readonly = val;
+    if (val) {
+      this.titlePrefix = this.l('label.readonly');
+    }
+  }
+
+  /** 只读 */
+  get readonly(): boolean {
+    return this._readonly;
+  }
+
+  /** 编辑 */
   isEdit: boolean;
-
 
   constructor(injector: Injector) {
     super(injector);
