507ac7c92e18357ae25c0dbb7da36732c03dba60
staneee <yi.hang@live.com>
2020-08-09 14:15:09 +0800

    增加debug helper

---
+++ src/Company.Project.Domain/Debugger/DebugHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.Debugger
+{
+    public static class DebugHelper
+    {
+#if DEBUG
+        public static bool IsDebug => true;
+#endif
+
+#if !DEBUG
+        public static bool IsDebug => false;
+#endif
+    }
+}
