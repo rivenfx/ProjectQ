62a85e8fef76a2ed93d4b7390690a707aa0d6d82
staneee <yi.hang@live.com>
2020-08-10 21:25:06 +0800

    增加动态排序参数

---
+++ src/Company.Project.Domain/DynamicQuery/SortCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.DynamicQuery
+{
+    /// <summary>
+    /// 排序表达式依据实体
+    /// </summary>
+    class SortCondition
+    {
+    }
+}
---
+++ src/Company.Project.Domain/DynamicQuery/SortType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.DynamicQuery
+{
+    /// <summary>
+    /// 排序类型
+    /// </summary>
+    public enum SortType
+    {
+        /// <summary>
+        /// 升序
+        /// </summary>
+        Asc = 0,
+        /// <summary>
+        /// 降序
+        /// </summary>
+        Desc = 1
+    }
+}
