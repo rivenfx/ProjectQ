614b3a29750807c25b6d7c84aa54e5d611857e3d
staneee <yi.hang@live.com>
2020-08-14 21:24:50 +0800

    修改错误的代码

--- ui/angular/src/app/shared/components/sample-components/sample-components.module.ts
+++ ui/angular/src/app/shared/components/sample-components/sample-components.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ServiceProxyModule } from '@service-proxies';
 import { SampleInputComponent } from '@shared/components/sample-components/sample-input';
 import { SHARED_ZORRO_MODULES } from '@shared/shared-zorro.module';
 import { SampleDataSourceService } from './sample-data-source.service';
@@ -14,7 +13,6 @@ const COMPONENTS = [
   imports: [
     CommonModule,
     FormsModule,
-    ServiceProxyModule,
     SHARED_ZORRO_MODULES
   ],
   declarations: [
