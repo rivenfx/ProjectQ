cea0e309db3c47bfded91aa3b7a691284e32e044
staneee <yi.hang@live.com>
2020-08-14 15:16:33 +0800

    查询输入增加动态查询条件和排序条件

--- src/Company.Project.Application/Dtos/QueryInput.cs
+++ src/Company.Project.Application/Dtos/QueryInput.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Company.Project.Dtos
@@ -9,8 +10,23 @@ namespace Company.Project.Dtos
     /// </summary>
     public class QueryInput
     {
+        /// <summary>
+        /// 页面数据总数
+        /// </summary>
         public int PageSize { get; set; }
-
+        /// <summary>
+        /// 跳过的数量
+        /// </summary>
         public int SkipCount { get; set; }
+
+        /// <summary>
+        /// 动态查询条件
+        /// </summary>
+        public List<QueryCondition> QueryConditions { get; set; }
+
+        /// <summary>
+        /// 动态排序
+        /// </summary>
+        public List<SortCondition> SortConditions { get; set; }
     }
 }
