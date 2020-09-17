2c83e85ac5cb15f45228f095e7299c65e028578a
staneee <yi.hang@live.com>
2020-09-17 23:29:58 +0800

    修改通知服务

--- ui/angular/src/app/shared/riven/notify.service.ts
+++ ui/angular/src/app/shared/riven/notify.service.ts
@@ -1,28 +1,57 @@
 import { Injectable } from '@angular/core';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import * as _ from 'loadsh';
 
 @Injectable()
 export class NotifyService {
+
+  protected _options = {
+    nzPlacement: 'bottomRight'
+  };
 
   constructor(
     public notification: NzNotificationService,
   ) {
   }
 
-  info(message: string, title?: string, options?: any): void {
-    this.notification.info(message, title, options);
+  info(message: string, title?: string, options: any = {}): void {
+    if (!options) {
+      options = {};
+    }
+    this.notification.info(message, title, {
+      ...options,
+      ...this._options
+    });
   }
 
-  success(message: string, title?: string, options?: any): void {
-    this.notification.success(message, title, options);
+  success(message: string, title?: string, options: any = {}): void {
+    if (!options) {
+      options = {};
+    }
+    this.notification.success(message, title, {
+      ...options,
+      ...this._options
+    });
   }
 
-  warn(message: string, title?: string, options?: any): void {
-    this.notification.warning(message, title, options);
+  warn(message: string, title?: string, options: any = {}): void {
+    if (!options) {
+      options = {};
+    }
+    this.notification.warning(message, title, {
+      ...options,
+      ...this._options
+    });
   }
 
-  error(message: string, title?: string, options?: any): void {
-    this.notification.error(message, title, options);
+  error(message: string, title?: string, options: any = {}): void {
+    if (!options) {
+      options = {};
+    }
+    this.notification.error(message, title, {
+      ...options,
+      ...this._options
+    });
   }
 
 }
