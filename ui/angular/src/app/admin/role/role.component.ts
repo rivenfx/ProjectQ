967e1bf7ec7d76694e65af2fe82f656e01e379b4
staneee <yi.hang@live.com>
2020-09-17 22:29:33 +0800

    修改用户和角色列表界面

--- ui/angular/src/app/admin/role/role.component.ts
+++ ui/angular/src/app/admin/role/role.component.ts
@@ -21,7 +21,10 @@ export class RoleComponent extends ListViewComponentBase<RoleDto>
   }
 
   ngOnInit(): void {
-
+    this.pageInfo.scroll = {
+      x: '600px',
+      y: '240px'
+    };
   }
 
   fetchData(fetch: IFetchPageData) {
