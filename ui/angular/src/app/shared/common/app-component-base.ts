40ddf6da926602b545b23847afe1d7f8d045ac0d
staneee <yi.hang@live.com>
2020-10-20 22:46:44 +0800

    修改循环依赖的问题

--- ui/angular/src/app/shared/common/app-component-base.ts
+++ ui/angular/src/app/shared/common/app-component-base.ts
@@ -1,10 +1,10 @@
-import { Inject, Injector } from '@angular/core';
-import { I18nService } from '@core/i18n';
-import { ALAIN_I18N_TOKEN, TitleService } from '@delon/theme';
+import { Injector } from '@angular/core';
+import { TitleService } from '@delon/theme';
 import { AppConsts } from '@shared/app-consts';
 import { MessageService } from '@shared/riven/message.service';
+import { NotifyService } from '@shared/riven/notify.service';
 import { SampleComponentBase } from './sample-component-base';
-import { NotifyService } from '@shared/riven';
+
 
 export abstract class AppComponentBase extends SampleComponentBase {
 
