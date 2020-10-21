8ea8c138600ccbd657f9b6b02c956b6b858bac8e
staneee <yi.hang@live.com>
2020-10-21 22:51:28 +0800

    修改错误的代码和命名

--- ui/angular/src/app/shared/common/sample-component-base.ts
+++ ui/angular/src/app/shared/common/sample-component-base.ts
@@ -9,23 +9,24 @@ export abstract class SampleComponentBase {
 
   i18nSer: I18nService;
 
-  permission: PermissionCheckerService;
+  permissionSer: PermissionCheckerService;
 
   constructor(
     public injector: Injector,
   ) {
     this.i18nSer = injector.get<I18nService>(ALAIN_I18N_TOKEN);
+    this.permissionSer = injector.get(PermissionCheckerService);
   }
 
   l(key: string, ...args: any[]): string {
     return this.i18nSer.fanyi(key, args);
   }
 
-  isGranted(permissionName: string | string[]): boolean {
+  isGranted(permissionName: string | string[], requireAll: boolean = false): boolean {
     if (!permissionName) {
       return true;
     }
 
-    return this.permission.isGranted(permissionName);
+    return this.permissionSer.isGranted(permissionName, requireAll);
   }
 }
