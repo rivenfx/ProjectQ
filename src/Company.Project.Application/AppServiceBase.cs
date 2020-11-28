14c5b71b82d19e1eb14f13e52f70c24bcbcf44a3
staneee <yi.hang@live.com>
2020-11-28 19:50:27 +0800

    增加AppServiceBase,接口基类

--- src/Company.Project.Application/AppServiceBase.cs
+++ src/Company.Project.Application/AppServiceBase.cs
@@ -1,6 +1,9 @@
 using System;
+using Company.Project.Authorization;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Riven.Application;
+using Riven.Localization;
 using Riven.Uow;
 
 namespace Company.Project
@@ -14,10 +17,17 @@ namespace Company.Project
             _serviceProvider = serviceProvider;
 
             this.UnitOfWorkManager = _serviceProvider.GetRequiredService<IUnitOfWorkManager>();
+            this.AppSession = _serviceProvider.GetRequiredService<IAppSession>();
+            this.Configuration = _serviceProvider.GetRequiredService<IConfiguration>();
+            this.LanguageManager = _serviceProvider.GetRequiredService<ILanguageManager>();
+            this.CurrentLanguage = _serviceProvider.GetRequiredService<ICurrentLanguage>();
         }
 
         protected virtual IUnitOfWorkManager UnitOfWorkManager { get; }
         protected virtual IActiveUnitOfWork CurrentUnitOfWork => UnitOfWorkManager.Current;
-
+        protected virtual IAppSession AppSession { get; }
+        protected virtual IConfiguration Configuration { get; }
+        protected virtual ILanguageManager LanguageManager { get; }
+        protected virtual ICurrentLanguage CurrentLanguage { get; }
     }
 }
