56048574495b1e4b41e5e91aa4e6aa738adc3997
staneee <yi.hang@live.com>
2020-12-02 20:34:16 +0800

    增加拆分 .  取最后一个字符串的本地化函数

--- ui/angular/src/app/shared/common/sample-component-base.ts
+++ ui/angular/src/app/shared/common/sample-component-base.ts
@@ -22,6 +22,15 @@ export abstract class SampleComponentBase {
     return this.i18nSer.fanyi(key, args);
   }
 
+  ls(key: string, ...args: any[]): string {
+    const lastIndex = key.lastIndexOf('.');
+    if (lastIndex !== -1) {
+      const tmpKey = key.substr(lastIndex + 1, key.length - lastIndex);
+      return this.l(tmpKey, args);
+    }
+    return this.l(key, args);
+  }
+
   isGranted(permissionName: string | string[], requireAll: boolean = false): boolean {
     if (!permissionName) {
       return true;
