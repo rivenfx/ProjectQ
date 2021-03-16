19152c819ffcecff3a49d4deb54baed6a5d00308
staneee <yi.hang@live.com>
2021-03-16 16:07:26 +0800

    获取当前连接字符串名称的实现修改

--- src/Company.Project.Host/Uow/AspNetCoreCurrentConnectionStringNameProvider.cs
+++ src/Company.Project.Host/Uow/AspNetCoreCurrentConnectionStringNameProvider.cs
@@ -1,17 +1,40 @@
+using Company.Project;
+
 using Microsoft.AspNetCore.Http;
 
 using Riven.MultiTenancy;
+using Riven.Extensions;
 
 
 namespace Riven.Uow
 {
-
-    public class AspNetCoreCurrentConnectionStringNameProvider : AspNetCoreMultiTenancyProvider, ICurrentConnectionStringNameProvider
+    public class AspNetCoreCurrentConnectionStringNameProvider : ICurrentConnectionStringNameProvider
     {
-        public AspNetCoreCurrentConnectionStringNameProvider(IHttpContextAccessor httpContextAccessor, IMultiTenancyOptions multiTenancyOptions) : base(httpContextAccessor, multiTenancyOptions)
+        protected readonly IHttpContextAccessor _httpContextAccessor;
+        protected readonly IMultiTenancyOptions _multiTenancyOptions;
+
+        public AspNetCoreCurrentConnectionStringNameProvider(IHttpContextAccessor httpContextAccessor, IMultiTenancyOptions multiTenancyOptions)
         {
+            _httpContextAccessor = httpContextAccessor;
+            _multiTenancyOptions = multiTenancyOptions;
         }
 
-        public string Current => this.CurrentTenantNameOrNull();
+        public string Current => this.GetRequestTenantName();
+
+        /// <summary>
+        /// 获取请求头中的租户名称
+        /// </summary>
+        /// <returns></returns>
+        protected virtual string GetRequestTenantName()
+        {
+            // 未启用多租户,则返回系统默认创建的租户名称
+            if (!_multiTenancyOptions.IsEnabled)
+            {
+                return AppConsts.MultiTenancy.DefaultTenantName;
+            }
+
+            // 启用多租户，返回当前请求头中携带的租户名称
+            return _httpContextAccessor?.HttpContext?.Request?.Headers?.GetTenantName();
+        }
     }
 }
