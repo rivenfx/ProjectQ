98b301e9b6a1fe6c9ff9c823657b10329e408ee0
staneee <yi.hang@live.com>
2021-03-17 22:18:41 +0800

    权限类添加

---
+++ src/Company.Project.Domain/Authorization/Permissions/Permission.cs
@@ -0,0 +1,24 @@
+﻿using Riven.Entities;
+using Riven.Identity.Permissions;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Project.Authorization.Permissions
+{
+    public class Permission : AppPermission, IEntity<Guid>, IMayHaveTenant
+    {
+        public virtual string TenantName { get; set; }
+
+        public virtual bool EntityEquals(object obj)
+        {
+            return EntityHelper.EntityEquals(this, obj);
+        }
+
+        public virtual bool IsTransient()
+        {
+            return EntityHelper.IsTransient(this);
+        }
+    }
+}
