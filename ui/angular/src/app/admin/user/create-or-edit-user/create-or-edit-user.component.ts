8c3950325814f1c3e814bbff4a2dceb4d294c1b8
staneee <yi.hang@live.com>
2021-06-14 17:57:26 +0800

    格式化代码

--- ui/angular/src/app/admin/user/create-or-edit-user/create-or-edit-user.component.ts
+++ ui/angular/src/app/admin/user/create-or-edit-user/create-or-edit-user.component.ts
@@ -1,14 +1,14 @@
-import { Component, Injector, OnInit } from "@angular/core";
-import { CreateOrEditUserInput, CreateOrUpdateRoleInput, UserDto, UserServiceProxy } from "@service-proxies";
-import { AppConsts } from "@shared";
-import { ModalComponentBase } from "@shared/common";
-import { finalize } from "rxjs/operators";
-import { SFSchema } from "@delon/form";
+import { Component, Injector, OnInit } from '@angular/core';
+import { CreateOrEditUserInput, CreateOrUpdateRoleInput, UserDto, UserServiceProxy } from '@service-proxies';
+import { AppConsts } from '@shared';
+import { ModalComponentBase } from '@shared/common';
+import { finalize } from 'rxjs/operators';
+import { SFSchema } from '@delon/form';
 
 @Component({
-  selector: "create-or-edit-user",
-  templateUrl: "./create-or-edit-user.component.html",
-  styleUrls: ["./create-or-edit-user.component.less"]
+  selector: 'create-or-edit-user',
+  templateUrl: './create-or-edit-user.component.html',
+  styleUrls: ['./create-or-edit-user.component.less']
 })
 export class CreateOrEditUserComponent extends ModalComponentBase<string>
   implements OnInit {
@@ -20,84 +20,84 @@ export class CreateOrEditUserComponent extends ModalComponentBase<string>
   passwordConfimd: string;
 
   schema: SFSchema = {
-    "properties": {
-      "userName": {
-        "type": "string",
-        "title": "账号",
-        "minLength": 5
+    properties: {
+      userName: {
+        type: 'string',
+        title: '账号',
+        minLength: 5
       },
-      "nickname": {
-        "type": "string",
-        "title": "昵称",
-        "minLength": 5
+      nickname: {
+        type: 'string',
+        title: '昵称',
+        minLength: 5
       },
-      "password": {
-        "title": "密码",
-        "type": "string",
-        "minLength": 5
+      password: {
+        title: '密码',
+        type: 'string',
+        minLength: 5
       },
-      "passwordConfimd": {
-        "title": "确认密码",
-        "type": "string",
-        "minLength": 5
+      passwordConfimd: {
+        title: '确认密码',
+        type: 'string',
+        minLength: 5
       },
-      "phoneNumber": {
-        "type": "string",
-        "title": "电话号码"
+      phoneNumber: {
+        type: 'string',
+        title: '电话号码'
       },
-      "email": {
-        "type": "string",
-        "title": "邮箱",
-        "format": "email"
+      email: {
+        type: 'string',
+        title: '邮箱',
+        format: 'email'
       },
-      "phoneNumberConfirmed": {
-        "title": "电话号码确认",
-        "type": "boolean",
-        "ui": {
-          "grid": {
-            "span": 6
+      phoneNumberConfirmed: {
+        title: '电话号码确认',
+        type: 'boolean',
+        ui: {
+          grid: {
+            span: 6
           }
         }
       },
-      "emailConfirmed": {
-        "title": "邮箱确认",
-        "type": "boolean",
-        "ui": {
-          "grid": {
-            "span": 6
+      emailConfirmed: {
+        title: '邮箱确认',
+        type: 'boolean',
+        ui: {
+          grid: {
+            span: 6
           }
         }
       },
-      "lockoutEnabled": {
-        "title": "登录锁定",
-        "type": "boolean",
-        "ui": {
-          "grid": {
-            "span": 6
+      lockoutEnabled: {
+        title: '登录锁定',
+        type: 'boolean',
+        ui: {
+          grid: {
+            span: 6
           }
         }
 
       },
-      "isActive": {
-        "title": "激活",
-        "type": "boolean",
-        "ui": {
-          "grid": {
-            "span": 6
+      isActive: {
+        title: '激活',
+        type: 'boolean',
+        ui: {
+          grid: {
+            span: 6
           }
         }
       }
     },
-    "required": ["email", "name"],
-    "ui": {
-      "errors": {
-        "minLength": this.l("validation.minlength"),
-        "maxLength": this.l("validation.maxlength"),
-        "required": this.l("validation.required")
+    required: ['email', 'name'],
+    ui: {
+      errors: {
+        minLength: this.l('validation.minlength'),
+        maxLength: this.l('validation.maxlength'),
+        required: this.l('validation.required')
       },
-      "spanLabelFixed": 100,
-      "grid": {
-        "span": 12
+      spanLabelFixed: 100,
+      grid: {
+        span: 12
       }
     }
   };
@@ -110,7 +110,7 @@ export class CreateOrEditUserComponent extends ModalComponentBase<string>
   }
 
   ngOnInit(): void {
-    this.title = this.l("user");
+    this.title = this.l('user');
     if (this.modalInput) {
       this.loading = true;
       this.userSer.getEditById(this.modalInput)
