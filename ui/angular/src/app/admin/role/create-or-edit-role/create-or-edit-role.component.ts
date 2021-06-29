ab8df95d6fb01411ff22c986468657856524e5b6
staneee <yi.hang@live.com>
2021-06-29 12:59:11 +0800

    编辑角色修改

--- ui/angular/src/app/admin/role/create-or-edit-role/create-or-edit-role.component.ts
+++ ui/angular/src/app/admin/role/create-or-edit-role/create-or-edit-role.component.ts
@@ -1,4 +1,5 @@
 import { Component, Injector, OnInit } from '@angular/core';
+import { SFSchema } from '@delon/form';
 import { CreateOrUpdateRoleInput, RoleDto, RoleServiceProxy } from '@service-proxies';
 import { AppConsts } from '@shared';
 import { AppComponentBase, ModalComponentBase } from '@shared/common';
@@ -13,7 +14,84 @@ export class CreateOrEditRoleComponent extends ModalComponentBase<string>
   implements OnInit {
 
   role = new RoleDto();
-  permissions: string[] = [];
+
+  permission = {
+    permissions: []
+  };
+
+
+
+  /** 基本表单配置 */
+  pageFormSchema: SFSchema = {
+    properties: {
+      name: {
+        type: 'string',
+        title: this.l('role.name'),
+        minLength: 5,
+      },
+      displayName: {
+        title: this.l('role.display-name'),
+        type: 'string',
+        minLength: 5,
+      },
+      description: {
+        title: this.l('role.description'),
+        type: 'string',
+        minLength: 5,
+        maxLength: 512,
+        ui: {
+          widget: 'textarea',
+          autosize: { minRows: 2, maxRows: 6 },
+          grid: {
+            span: 24,
+          },
+        }
+      },
+    },
+    required: [
+      'name',
+      'displayName',
+      'description'
+    ],
+    ui: {
+      errors: {
+        minLength: this.l('validation.minlength'),
+        maxLength: this.l('validation.maxlength'),
+        required: this.l('validation.required'),
+        confirm: this.l('validation.confirm')
+      },
+      spanLabelFixed: 100,
+      grid: {
+        span: 12,
+      },
+    },
+  };
+
+
+  /** 权限表单配置 */
+  permissionFormSchema: SFSchema = {
+    properties: {
+      permissions: {
+        type: 'string',
+        title: '',
+        ui: {
+          widget: 'custom'
+        },
+      },
+    },
+    required: [],
+    ui: {
+      errors: {
+        minLength: this.l('validation.minlength'),
+        maxLength: this.l('validation.maxlength'),
+        required: this.l('validation.required'),
+      },
+      spanLabelFixed: 1,
+      grid: {
+        span: 24,
+      },
+    },
+  };
 
   constructor(
     injector: Injector,
@@ -32,43 +110,46 @@ export class CreateOrEditRoleComponent extends ModalComponentBase<string>
         }))
         .subscribe((res) => {
           this.role = res.entityDto;
-          this.permissions = res.permissions;
+          this.permission = {
+            permissions: res.permissions
+          };
 
           if (!this.readonly) {
             this.readonly = this.role.isStatic;
           }
-
-          // this.disableFormControls();
+          this.pageFormSchema.properties.name.readOnly = true;
         });
     }
   }
 
-  submitForm(event?: any) {
+  submitForm(...event: any[]) {
+    // 构建数据
     const input = new CreateOrUpdateRoleInput({
-      entityDto: this.role,
-      permissions: this.permissions,
+      entityDto: RoleDto.fromJS(event[0]),
+      permissions: event[1].permissions
     });
+    debugger
 
-    this.loading = true;
-    if (this.modalInput) {
-      this.roleSer.update(input)
-        .pipe(finalize(() => {
-          this.loading = false;
-        }))
-        .subscribe(() => {
-          this.notify.success(this.l(AppConsts.message.success));
-          this.success();
-        });
-    } else {
-      this.roleSer.create(input)
-        .pipe(finalize(() => {
-          this.loading = false;
-        }))
-        .subscribe(() => {
-          this.notify.success(this.l(AppConsts.message.success));
-          this.success();
-        });
-    }
+    // this.loading = true;
+    // if (this.modalInput) {
+    //   this.roleSer.update(input)
+    //     .pipe(finalize(() => {
+    //       this.loading = false;
+    //     }))
+    //     .subscribe(() => {
+    //       this.notify.success(this.l(AppConsts.message.success));
+    //       this.success();
+    //     });
+    // } else {
+    //   this.roleSer.create(input)
+    //     .pipe(finalize(() => {
+    //       this.loading = false;
+    //     }))
+    //     .subscribe(() => {
+    //       this.notify.success(this.l(AppConsts.message.success));
+    //       this.success();
+    //     });
+    // }
 
   }
 }
